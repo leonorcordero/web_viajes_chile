95dd52edeff2066724b3c4f5786d9cc0309504d4
leonorcordero <corderoleonor.r@gmail.com>
2024-07-20 00:41:21 -0400

    documentación del script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,3 @@
+// codigo de iniciacilizaciòn de tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
